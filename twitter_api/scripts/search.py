a44ad3474a165544400674eec5e72df6710d6595
Tom Karaffa <tom.karaffa@protonmail.com>
2021-10-21 21:36:52 -0700

    cleanup, add a few more arguments

--- twitter_api/scripts/search.py
+++ twitter_api/scripts/search.py
@@ -1,32 +1,51 @@
 import argparse
-import json
 
 from twitter_api.twitter_api import TwitterAPI
 
 
-def get_args():
-    parser = argparse.ArgumentParser("Search for Tweets based on a term.")
-    parser.add_argument("query", type=str)
+def check_json(file: str):
+    if not file.endswith(".json"):
+        print("Please use .json file extensions.")
+        raise TypeError
+    return file
 
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        prog="search.py",
+        description="Search for Tweets based on a term.",
+    )
+    parser.add_argument(
+        "query",
+        type=str,
+        help="The search term.",
+    )
+    parser.add_argument(
+        "--n_pages",
+        type=int,
+        help="The number of pages of Tweets to pull.",
+        required=False,
+        default=5,
+    )
+    parser.add_argument(
+        "--outfile",
+        type=check_json,
+        help="The name of the file in which to save results.",
+        required=False,
+    )
     return parser.parse_args()
 
 
 def search():
     args = get_args()
     query = args.query
+    n_pages = args.n_pages
+    outfile = args.outfile
 
     t = TwitterAPI()
-
-    tweets = list()
-
-    results = t.perform_search(query)
-    tweets.extend(results.get("data"))
-    for _ in range(5):
-        next_token = results.get("meta").get("next_token")
-        results = t.perform_search(query, next_token=next_token)
-        tweets.extend(results.get("data"))
-    with open("results.json", "w+") as f:
-        json.dump(tweets, f, sort_keys=True, indent=2)
+    results = t.paginate_tweets(query, n_pages=n_pages)
+    filename = outfile or f"{query}.json"
+    t.write_json_file(results, filename)
 
 
 if __name__ == "__main__":
